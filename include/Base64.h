693004cf5cab2d2a62f853e178fb568a446c491d
Egor Lord <egor.lord-127@yandex.com>
2017-08-22 10:39:59 -0700

    Fixed base64 function declaration

--- include/Base64.h
+++ include/Base64.h
@@ -1,2 +1,2 @@
-int base64_encode(unsigned char* in, char* out, unsigned int in_len)
-char* base64_decode(const char*);
+int base64_encode(const unsigned char* in, unsigned char* out, unsigned int in_len);
+int base64_decode(const unsigned char *in, unsigned char *out, unsigned int in_len);
