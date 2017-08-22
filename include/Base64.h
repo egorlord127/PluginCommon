b41be00fbf5f36f0d4fd033b11109c2f55323f55
Egor Lord <egor.lord-127@yandex.com>
2017-08-22 11:38:28 -0700

    add util

--- include/Base64.h
+++
@@ -1,2 +0,0 @@
-int base64_encode(const unsigned char* in, unsigned char* out, unsigned int in_len);
-int base64_decode(const unsigned char *in, unsigned char *out, unsigned int in_len);
---
+++ include/Util.h
@@ -0,0 +1,3 @@
+int base64_encode(const unsigned char* in, unsigned char* out, unsigned int in_len);
+int base64_decode(const unsigned char *in, unsigned char *out, unsigned int in_len);
+void generateSecureRandomString(char *s, const int length);
