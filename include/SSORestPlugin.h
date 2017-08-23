840f1c98636a23aa391322c45bb6b1c480d8bc59
Egor Lord <egor.lord-127@yandex.com>
2017-08-23 09:56:25 -0700

    define pool functions

--- include/SSORestPlugin.h
+++ include/SSORestPlugin.h
@@ -1,13 +1,17 @@
 #pragma once
-
 #include "Global.h"
+
 typedef struct SSORestPlugin SSORestPlugin;
 typedef struct SSORestPluginConfigration SSORestPluginConfigration;
 
 #ifdef APACHE
-typedef apr_pool_t SSORestPluginPool;
+    typedef apr_pool_t SSORestPluginPool;
+    #define ssorest_pcalloc(pool, size) (apr_pcalloc(pool, size))
+    #define ssorest_palloc(pool, size) (apr_palloc(pool, size))
 #elif NGINX
-typedef ngx_pool_t SSORestPluginPool;
+    typedef ngx_pool_t SSORestPluginPool;
+    #define ssorest_pcalloc(pool, size) (ngx_pcalloc(pool, size))
+    #define ssorest_palloc(pool, size) (ngx_palloc(pool, size))
 #endif
 
 struct SSORestPlugin
@@ -16,8 +20,8 @@ struct SSORestPlugin
     void (*createPluginConfiguration)(SSORestPlugin*, SSORestPluginPool*);
 
 };
-void* (*ssorest_pcalloc)(SSORestPluginPool*, size_t);
-void* (*ssorest_palloc)(SSORestPluginPool*, size_t);
+// void* (*ssorest_pcalloc)(SSORestPluginPool*, size_t);
+// void* (*ssorest_palloc)(SSORestPluginPool*, size_t);
 struct SSORestPluginConfigration
 {
     int isEnabled;
--- src/SSORestPlugin.c
+++ src/SSORestPlugin.c
@@ -1,8 +1,18 @@
 #include "SSORestPlugin.h"
-#include <apr_pools.h>
+#include "apr_hash.h"
+#include "ap_config.h"
+#include "ap_provider.h"
+#include "httpd.h"
+#include "http_core.h"
+#include "http_config.h"
+#include "http_log.h"
+#include "http_protocol.h"
+#include "http_request.h"
+#include "apr_optional.h"
+
+
 void createPluginConfiguration(SSORestPlugin* self, SSORestPluginPool* pool)
 {
-    // self->pluginConfiguration = apr_pcalloc(pool, sizeof(SSORestPluginConfigration));
-    ssorest_palloc = &apr_palloc;
-    self->pluginConfiguration = ssorest_palloc(pool, sizeof(SSORestPluginConfigration));
+    self->pluginConfiguration = ssorest_pcalloc(pool, sizeof(SSORestPluginConfigration));
 }
+
